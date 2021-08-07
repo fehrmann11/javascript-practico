7651c5ea5b70b7838d06548e2f0d85fb24898090
fehrmann11 <cajapasto5@gmail.com>
2021-08-07 18:54:22 -0400

    taller 2 listo

--- descuentos.js
+++ descuentos.js
@@ -1,15 +1,32 @@
-const precioOriginal = 100;
-const descuento = 15;
-
 function calcularPrecioConDescuento(){
+    const cupones = ['henry','soyLoMaximo','free'];
     const inputPrice = document.getElementById('price').value;
-    const inputDescount = document.getElementById('descount').value;
-    const result = document.getElementById('result')
-    const porcentajePrecioDescuento = 100 - inputDescount;
+    const inputDescount = document.getElementById('cupones').value;
+    const result = document.getElementById('result');
+    let descount;
+    switch (inputDescount) {
+        
+        case cupones[0]:
+            console.log(inputDescount)
+            descount = 19;
+            break;
+        case cupones[1]:
+            descount = 7;
+            break
+        case cupones[2]:
+            console.log(inputDescount)
+            descount = 100;
+            break
+        default:
+            descount = 0;
+    }
+    let porcentajePrecioDescuento = 100 - descount;
+    console.log(porcentajePrecioDescuento)
     const precioConDescuento = (inputPrice * porcentajePrecioDescuento)/100;
     //escribir en javascript
     return result.innerHTML = `${precioConDescuento}`
 }
+
 
 
 
